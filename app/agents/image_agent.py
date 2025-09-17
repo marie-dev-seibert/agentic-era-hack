1140118155d233f69921bce11cdb60416ec1c86e
Marie Scharhag <mscharhag@seibert-media.net>
2025-09-17 17:59:34 +0200

    improve prompt and save images in bucket

--- app/agents/image_agent.py
+++ app/agents/image_agent.py
@@ -4,9 +4,10 @@ from app.tools.generate_image_tool import generate_image
 
 
 image_agent = Agent(
-    name="thumbnail_agent",
+    name="image_agent",
     model="gemini-2.5-flash",
     instruction=inital_image_prompt,
+    description="You are an AI Agent that generates image for a podcast and iterate with the user until the user is satisfied. After Image creation ask the user if he wants to continue with the podcast generation process and exit to your parent agent",
     tools=[generate_image],
-    output_key="output_image",
+    # output_key="output_image",
 )
