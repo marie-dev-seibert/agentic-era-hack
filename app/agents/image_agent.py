1fe4e70419bcbe8d437b10d4e435711f5f267e5e
Marie Scharhag <mscharhag@seibert-media.net>
2025-09-17 16:06:03 +0200

    merge

--- app/agents/image_agent.py
+++ app/agents/image_agent.py
@@ -1,14 +1,7 @@
-import os
-import google.auth
 from google.adk.agents import Agent
 from app.prompts.inital_image_prompt import inital_image_prompt
 from app.tools.generate_image_tool import generate_image
 
-
-_, project_id = google.auth.default()
-os.environ.setdefault("GOOGLE_CLOUD_PROJECT", project_id)
-os.environ.setdefault("GOOGLE_CLOUD_LOCATION", "global")
-os.environ.setdefault("GOOGLE_GENAI_USE_VERTEXAI", "True")
 
 image_agent = Agent(
     name="thumbnail_agent",
---
+++ app/prompts/__init__.py
@@ -0,0 +1,3 @@
+from .inital_image_prompt import inital_image_prompt
+
+__all__ = ["inital_image_prompt"]
